ebb8475efa239cb2a910b5e4cd2f5c334d97fd71
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-01-20 18:24:11 -0800

    Update deck.py shuffleDeck method

--- deck.py
+++ deck.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import randint
 from card import Card
 
 class Deck:
@@ -18,23 +18,35 @@ class Deck:
         13: "King"
     }
     
-    _suits = ("Hearts", "Diamonds", "Clubs", "Spades")
+    _suits = ("Hearts", "Clubs", "Diamonds", "Spades")
     
     def __init__(self):
         self.deck = self.generateDeck()
+        self.length = len(self.deck)
+        self.shuffled = False
         
     def generateDeck(self):
         full_deck = []
 
         for suit in self._suits:
-            for card_value in self._cards.values():
-                newCard = Card(suit, card_value)
+            for card_value in self._cards.keys():
+                newCard = Card(suit, self._cards[card_value], card_value)
                 full_deck.append(newCard)
+
         return full_deck
 
     def shuffleDeck(self):
-        return shuffle(self.deck)
+        shuffledDeck = []
+
+        while len(self.deck) > 0:
+            i = randint(0, len(self.deck) - 1)
+            shuffledDeck.append(self.deck.pop(i))
+        
+        self.deck = shuffledDeck
+        self.shuffled = True
 
     def showDeck(self):
+        if(self.shuffled):
+            print("\nShuffled deck:")
         for card in self.deck:
-            print(f"{card.value} of {card.suit}")
+            print(f"{card.card_type} of {card.suit} with a value of {card.value}")
