0d03a9e61d33d5a34a93d4a0cba2b52f751ddceb
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-02-17 18:54:03 -0800

    Add constructor and work on drawCard method

--- hand.py
+++ hand.py
@@ -2,17 +2,11 @@ from deck import Deck
 
 class Hand(Deck):
     def __init__(self):
+        super().__init__()
         self.deck = []
-        self.shuffled = False
-        self.sorted = False
-
-    def buildHand(self, gameType, currentDeck):
-        if gameType == "BlackJack":
-            self.drawCard(currentDeck, 2)
     
-    def drawCard(self, currentDeck: Deck, numOfCards: int):
+    def drawCard(self, currentDeck: Deck, numOfCards: int, hand: list):
         for card in range(0, numOfCards):
-            self.deck.append(currentDeck.deck.pop(0))
-            self.length = len(self.deck)
+            hand.append(currentDeck.deck.pop(0))
 
-    
+        return hand
