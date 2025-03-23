37358654f2658fcfbedcdf18abfae048571a5d61
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-03-23 00:04:30 -0700

    Correct required_version to be a tuple instead of a set

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from blackjack import BlackJack
 import sys
 
 # Python version check (3.10 or higher)
-required_version = {3, 10}
+required_version = (3, 10)
 if sys.version_info < required_version:
     print(f"Error: Please use Python {required_version[0]}.{required_version[1]} or later.")
     sys.exit(1)
