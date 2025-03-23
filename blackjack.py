0851f45001430d0dfa9cde76d81586c825710ba3
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-03-23 13:20:38 -0700

    Transition busted and stand values Move to be class attributes

--- blackjack.py
+++ blackjack.py
@@ -17,17 +17,18 @@ class BlackJack():
         self.newDealer.drawCard(self.newDeck, 2)
 
         # Start Player's turn
-        playerStand = False
-        while not playerStand:
+        while not self.newPlayer.playerStand:
             self.newPlayer.playerHand.showHand()
             self.checkHandValue(self.newPlayer)
 
             # Check for valid input
             while True:
                 try:
-                    choice = int(input("Hit (1) or Stand (2) "))
+                    choice = int(input("Hit (1) or Stand (2) or Show Card Value (3) "))
                     if choice == 1 or choice == 2:
                         break
+                    elif choice == 3:
+                        print(self.newPlayer.getHandValue(self.newPlayer.playerHand))
                     else:
                         raise ValueError()
                 except ValueError:
@@ -38,16 +39,15 @@ class BlackJack():
                 case 1:
                     self.newPlayer.drawCard(self.newDeck, 1)
                 case 2:
-                    print("You chose to stand")
-                    playerStand = True
+                    print("You chose to stand.")
+                    self.newPlayer.playerStand = True
 
 
     def checkHandValue(self, player):
-        handValue = 0
-        for card in player.playerHand.cards:
-            handValue += card.value
+        handValue = player.getHandValue(player.playerHand)
         
         if handValue == 21:
             print("Blackjack!")
         elif handValue >= 22:
             print("You busted :/")
+            player.playerBusted = True
--- player.py
+++ player.py
@@ -3,7 +3,14 @@ from hand import Hand
 class Player():
     def __init__(self, hand):
         self.playerHand = hand
+        self.playerBusted = False
+        self.playerStand = False
 
     def drawCard(self, deck, numOfCards):
         for _ in range(numOfCards):
             self.playerHand.addCard(deck.deck.pop())
+
+    def getHandValue(self, hand):
+        handValue = sum(card.value for card in self.playerHand.cards)
+
+        return handValue
