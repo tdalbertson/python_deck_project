d81d4820803d1a72c54bb398dda1e0df9dd56652
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-03-22 23:58:53 -0700

    Update player's turn:
    1. Change print to show player's hand to showHand method
    2. Add input validation
    3. Add hit and stand logic
    
    TODO:
    1. Add game end when busting a hand
    2. Add break when choosing to stand
    3. Add option to display total card value
    4. Handle ace behavior: If counting the Ace as 11
    would cause the hand to exceed 21, it's counted as 1.

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,7 @@ from deck import Deck
 from hand import Hand
 from player import Player
 from dealer import Dealer
+import os
 
 class BlackJack():
     def __init__(self):
@@ -16,18 +17,37 @@ class BlackJack():
         self.newDealer.drawCard(self.newDeck, 2)
 
         # Start Player's turn
-        self.checkHandValue(self.newPlayer.playerHand)
+        playerStand = False
+        while not playerStand:
+            self.newPlayer.playerHand.showHand()
+            self.checkHandValue(self.newPlayer)
 
-        print(str(self.newPlayer.playerHand))
-        print(self.newDealer.playerHand)
+            # Check for valid input
+            while True:
+                try:
+                    choice = int(input("Hit (1) or Stand (2) "))
+                    if choice == 1 or choice == 2:
+                        break
+                    else:
+                        raise ValueError()
+                except ValueError:
+                    os.system('clear')
+                    print("Invalid input. Please enter either a 1 or 2")
+            
+            match choice:
+                case 1:
+                    self.newPlayer.drawCard(self.newDeck, 1)
+                case 2:
+                    print("You chose to stand")
+                    playerStand = True
+
 
     def checkHandValue(self, player):
         handValue = 0
-        for card in player.hand:
-            print(card)  
-            # handValue += card.value
+        for card in player.playerHand.cards:
+            handValue += card.value
         
-        # if handValue == 21:
-        #     print("Blackjack!")
-        # elif handValue >= 22:
-        #     print("You busted :/")
+        if handValue == 21:
+            print("Blackjack!")
+        elif handValue >= 22:
+            print("You busted :/")
